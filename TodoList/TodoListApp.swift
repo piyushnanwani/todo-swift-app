11aefd4bc2a37a291846c98d869e040c76ae040c
Piyush Nanwani <pnanwani61@gmail.com>
2024-09-17 19:21:04 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Piyush Nanwani on 17/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
